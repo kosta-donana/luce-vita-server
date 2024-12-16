f3798461ab7866ba9e1b9ab26f011c126a34e69b
Claire <nbr1023@naver.com>
2024-12-16 17:27:48 +0900

    feature: 회원가입 및 로그인 최종 테스트 완료

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -3,14 +3,14 @@ import supabase from "../supabaseClients";
 class UserModel {
   // 사용자 이메일 조회
   async fetchUserEmail(email: string) {
-    const { data, error } = await supabase.from("user_info").select("email");
+    const { data, error } = await supabase.from("user_info").select("user_email").eq("user_eamil", email);
 
     if (error) {
       console.error("유저 데이터 DB 조회 실패", error);
       return [];
     }
 
-    return data ? data.map(row => row.email) : [];
+    return data ? data.map(row => row.user_email) : [];
   }
 
   // 사용자 정보와 여권 정보 같이 조회하는 함수
