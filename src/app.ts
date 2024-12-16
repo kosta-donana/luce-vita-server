6ba7c91c48c67536ee5db2648ad208efe79baeb2
zoe <gi@naver.com>
2024-12-16 17:01:33 +0900

    feat: CORS 요청 허용을 위한 미들웨어 추가

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,20 @@
+import cors from "cors";
 import dotenv from "dotenv";
-import express, { Application, Request, Response } from 'express';
-import userRoutes from './routes/userRouter';
-const app: Application = express()
+import express, { Application, Request, Response } from "express";
+import userRoutes from "./routes/userRouter";
+const app: Application = express();
+const PORT: number = Number(process.env.PORT) || 3000;
 dotenv.config();
-const PORT: number = Number(process.env.PORT)|| 3000;
+app.use(express.json());
+app.use(cors());
 
-app.use('/user', userRoutes);
-app.get('/toto', (req: Request, res: Response) => {
-    res.send('Hello toto')
-})
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello toto')
-})
+// 라우터 등록
+app.use("/users", userRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello toto");
+});
 
 app.listen(PORT, function () {
-    console.log(`App is listening on port ${PORT} !`)
-})
+  console.log(`App is listening on port ${PORT} !`);
+});
