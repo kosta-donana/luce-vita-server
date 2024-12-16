80dc0301d10a93aa175d4104dae44177b4f700fe
zoe <gi@naver.com>
2024-12-16 17:04:02 +0900

    feat: is_deleted 필드 true로 업데이트하여 사용자 탈퇴 기능 추가

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,24 +1,32 @@
 import supabase from "./supabaseClients";
 
 class UserModel {
+  // 사용자 정보와 여권 정보 같이 조회하는 함수
+  async findUserWithPassportById(userId: string) {
+    const { data, error } = await supabase.from("user").select("*, passport(*)").eq("id", userId);
 
-    // 사용자 정보와 여권 정보 같이 조회하는 함수
-    async findUserWithPassportById(userId : string) {
-
-        const { data, error } = await supabase
-            .from('user')
-            .select('*, passport(*)')
-            .eq('id', userId);
-        
-        if (error) {
-            throw new Error(error.message);
-        }
-
-        return data;
+    if (error) {
+      throw new Error(error.message);
     }
 
+    return data;
+  }
+
+  // 회원 탈퇴
+  async deleteUserById(user_id: string) {
+    console.log(user_id);
+    const { data, error } = await supabase
+      .from("user_info")
+      .update({ is_deleted: true })
+      .eq("user_id", user_id)
+      .select("*");
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return data;
+  }
 }
 
 const userModel = new UserModel();
 export { userModel };
-
