ef381ed05a1d23ada0bc011a4e9f9aed448c3daa
zoe <gi@naver.com>
2024-12-16 17:05:31 +0900

    feat: user_id로 사용자 탈퇴 처리하는 서비스 메서드 추가

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,16 +1,24 @@
-import { userModel } from '../models/userModel';
+import { userModel } from "../models/userModel";
 
 class UserService {
+  async getUserWithPassportById(userId: string) {
+    const userWithPassport = await userModel.findUserWithPassportById(userId);
 
-    async getUserWithPassportById(userId : string) {
-        const userWithPassport = await userModel.findUserWithPassportById(userId);
-
-        if (!userWithPassport || userWithPassport.length === 0) {
-            throw new Error('User Info not found');
-        }
-        return userWithPassport; 
+    if (!userWithPassport || userWithPassport.length === 0) {
+      throw new Error("User Info not found");
     }
+    return userWithPassport;
+  }
 
+  // 회원 탈퇴
+  async deleteUser(user_id: string) {
+    const deletedUser = await userModel.deleteUserById(user_id);
+    console.log(deletedUser);
+    if (!deletedUser || deletedUser.length === 0) {
+      throw new Error("User not found");
+    }
+    return deletedUser;
+  }
 }
 
 const userService = new UserService();
