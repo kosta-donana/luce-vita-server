3188eba77974a193ab4894fe1dd84202b317995d
2mandoo <dlwjdgus7441@gmail.com>
2024-12-26 15:35:41 +0900

    배포 수정

--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 
@@ -10,12 +11,19 @@ const swaggerOptions = {
             version: "1.0.0",
             description: "My API documentation",
         },
-        servers: [{ url: "http://localhost:4000" }],
+        servers: [{
+            url: process.env.NODE_ENV === 'production'
+                ? "https://your-production-url.vercel.app"
+                : "http://localhost:4000"
+        }],
     },
-    apis: ["./src/swagger/routes/*.swagger.ts",
-        "./src/swagger/components/*.ts"], // 주석을 포함한 파일 경로
+    apis: [
+        path.join(__dirname, './routes/*.swagger.{js,ts}'),
+        path.join(__dirname, './components/*.{js,ts}')
+    ], // 주석을 포함한 파일 경로
 };
 
+console.log("swagger apis path", __dirname);
 const specs = swaggerJSDoc(swaggerOptions);
 
 
