b8c0153260726b8ad88f636b85999e13f83a169e
Claire <nbr1023@naver.com>
2024-12-27 13:32:36 +0900

    Util: 인스턴스 에러 상태코드 409 Conflict로 수정

--- src/utils/errorHandle.ts
+++ src/utils/errorHandle.ts
@@ -3,7 +3,7 @@ import { Response } from "express";
 // 에러 핸들링 유틸리티
 const handleError = (res: Response, error: unknown) => {
   if (error instanceof Error) {
-    res.status(404).json({ success: false, error: error.message });
+    res.status(409).json({ success: false, error: error.message });
   } else {
     res.status(500).json({ success: false, error: String(error) });
   }
