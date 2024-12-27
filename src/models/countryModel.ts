335742e2edd6458faeba53dc7fe7ee7354cccf03
2mandoo <dlwjdgus7441@gmail.com>
2024-12-27 16:13:06 +0900

    country 정렬 수정

--- src/models/countryModel.ts
+++ src/models/countryModel.ts
@@ -5,10 +5,7 @@ class CountryModel {
     // 국가 정보 조회하는 함수
     async getCountryList() {
 
-        const { data, error } = await supabase
-            .from('country')
-            .select('*')
-            .order('country_name');
+        const { data, error } = await supabase.rpc('get_countries_sorted');
 
         if (error) {
             throw new Error(error.message);
@@ -20,3 +17,4 @@ class CountryModel {
 
 const countryModel = new CountryModel();
 export { countryModel };
+
