1cf2e105e2930ea33428e9ca178e23152854e7ed
koreandroid <dlwotjd116@snu.ac.kr>
2024-12-30 20:28:38 +0900

    Update app.ts
    배포된 주소로 변경

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const PORT: number = Number(process.env.PORT) || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://luce-vita-client.vercel.app",
     credentials: true,
 }));
 
